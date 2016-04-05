bcf41874db5c71a95aa8050c7d853c7d65c6d966
Aaron <aaron7899@Aarons-MacBook-Pro-2.local>
2016-04-04 21:33:18 -0400

    I changed this file and added the fctn and url

--- aaronApt.py
+++ aaronApt.py
@@ -1,5 +1,1 @@
-from bs4 import BeautifulSoup
-from time import sleep
- 
- def avaloncrwaler():
- 	url = "http://www.avaloncommunities.com/virginia/reston-herndon-apartments/avalon-reston-landing"
+from bs4 import BeautifulSoupfrom time import sleep  def avaloncrwaler(): 	url = "http://www.avaloncommunities.com/virginia/reston-herndon-apartments/avalon-reston-landing"    source_code = requests.get(url)    plain_txt = source_code.text    soup = BeautifulSoup(plain_txt)       
